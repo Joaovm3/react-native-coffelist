6955f56171a104d7e8ef26b92dbf309cbbd221f3
João Victor Martins <joao-victor.martins@gft.com>
2022-04-19 18:29:37 -0300

    chore: adding flat list coffe component

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, StatusBar } from 'react-native';
+import { CoffeeHeader } from "./src/components/CoffeeHeader";
+import { CoffeeItem } from "./src/components/CoffeeItem";
+import { SeparatorItem } from "./src/components/SeparatorItem";
+import { Coffee, coffeeList } from "./src/mocks/coffeeList";
 
 export default function App() {
+  const renderItem = ({ item }) => {
+    return <CoffeeItem {...item} />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar hidden={true}/>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponent={CoffeeHeader}
+        ItemSeparatorComponent={SeparatorItem}
+        data={coffeeList}
+        keyExtractor={(item) => item.name}
+        renderItem={renderItem}
+      />
     </View>
   );
 }
